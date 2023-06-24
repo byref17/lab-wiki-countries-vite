799954ba9282a0113b075b83f5386fd4ee3e475b
byref17 <124827023+byref17@users.noreply.github.com>
2023-06-24 11:24:43 +0200

    lab end

--- src/pages/CountryDetailsPage.jsx
+++ src/pages/CountryDetailsPage.jsx
@@ -1,3 +1,62 @@
-function CountryDetails() {}
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
+function CountryDetails() {
+    const [country, setCountry] = useState(null);
+
+    const { countryId } = useParams();
+
+    useEffect(() => {
+        axios.get(`https://ih-countries-api.herokuapp.com/countries/${countryId}`)
+            .then((response) => {
+                console.log(response.data);
+                setCountry(response.data);
+            })
+            .catch((err) => console.log(err));
+    }, []);
+    return (
+        <>
+            <div className="container">
+                <p style={{ fontSize: "24px", fontWeight: "bold" }}>Country Details</p>
+                {country && (
+                    <>
+                        <h1>{country.name.common}</h1>
+
+                        <table className="table">
+                            <thead></thead>
+                            <tbody>
+                                <tr>
+                                    <td style={{ width: "30%" }}>Capital</td>
+                                    <td>{country.capital}</td>
+                                </tr>
+                                <tr>
+                                    <td>Area</td>
+                                    <td>
+                                        {country.area} km
+                                        <sup>2</sup>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Borders</td>
+                                    <td>
+                                        <ul>
+
+                                            {country.borders.map((border) => {
+                                                return (
+                                                    <li key={border}><a href={`/${border}`}>{border}</a></li>
+                                                );
+                                            })}
+                                        </ul>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </>
+                )}
+            </div>
+        </>
+    );
+}
 export default CountryDetails;
